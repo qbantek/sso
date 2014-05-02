a706f8c12bb658f2166a6236729692282a871ce4
Erich Quintero <qbantek@gmail.com>
2014-05-02 05:16:02 -0700

    Initial commit
    ?
    
    unit test project was missing :S
    
    user token property on identity
    
    missing files
    
    removing Fin Apps Token - using config instead
    
    proj => added service
    
    fixed bug where account controller had no parameterless ctor

---
+++ SSO/Models/IdentityModels.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace SSO.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PostalCode { get; set; }
+        public string FinAppsUserToken { get; set; }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection")
+        {
+        }
+    }
+}
